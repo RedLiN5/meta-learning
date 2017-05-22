9b4b8ad6306a1d778b93f58b53123905b1c12fb2
Leslie_Yan <leslieyan0621@gmail.com>
2017-05-22 18:24:46 +0800

    [Fix] Fix condition of no duplicate in max_arg. Inputs of DataMatch. Run process

--- data_match.py
+++ data_match.py
@@ -18,10 +18,17 @@ def _select_candicate_input_short(series_row, candidates_list):
 
 def _select_candicate_input_long(score_df):
     max_arg = score_df.apply(lambda x: x.argmax())
+    max_arg.dropna(inplace=True)
     count = collections.Counter(max_arg).items()
     dup = [_item for _item, _count in count if _count > 1]
     uni = [_item for _item, _count in count if _count == 1]
-    col = max_arg.index[~max_arg.duplicated(keep=False)].tolist()
+    if len(np.unique(max_arg)) == len(max_arg):
+        col = max_arg.index.tolist()
+    else:
+        col = []
+        for value in np.unique(max_arg):
+            pos = max_arg == value
+            col.append(max_arg.index[pos][0])
     max_value = score_df.max().loc[col].tolist()
     for _row in dup:
         try:
@@ -35,13 +42,13 @@ def _select_candicate_input_long(score_df):
 
 class DataMatch(object):
 
-    def __init__(self, ):
-        fr = FeatureReader(X=None, y=None,
-                           id='test0',
-                           numeric_names=None,
-                           category_names=None,
-                           industry=None,
-                           business_func=None)
+    def __init__(self, X=None, y=None,id='test0',numeric_names=None,
+                 category_names=None, industry=None, business_func=None):
+        fr = FeatureReader(X=X, y=y, id=id,
+                           numeric_names=numeric_names,
+                           category_names=category_names,
+                           industry=industry,
+                           business_func=business_func)
         self.features_info = fr.run()
 
     def _load_metabase(self):
@@ -86,7 +93,7 @@ class DataMatch(object):
             candidate_scores = []
             for i in range(len(input_colnames)):
                 if len(candidates) < len(base_colnames):
-                    row = score_table.loc[i]
+                    row = score_table.ix[i,:]
                     name, value = _select_candicate_input_short(series_row=row,
                                                                 candidates_list=candidates)
                     candidates.append(name)
@@ -106,5 +113,10 @@ class DataMatch(object):
         return ids, scores
 
     def run(self, best_match=5):
-        pass
+        ids, scores = self._calculate_scores()
+        sorted_scores = sorted(((v, k) for (k, v) in enumerate(scores)),
+                               reverse=True)
+        index = list(map(lambda x: x[1], sorted_scores[:best_match]))
+        ids_best = np.array(ids)[index]
+        print('End:', ids_best)
 
