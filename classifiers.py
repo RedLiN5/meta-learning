40eb921514be6a1fb0993fe0affdbedbd18e7b60
Leslie_Yan <leslieyan0621@gmail.com>
2017-05-25 17:31:04 +0800

    Classifier and regressor module

---
+++ classifiers.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import logistic
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
---
+++ regressors.py
@@ -0,0 +1,7 @@
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
