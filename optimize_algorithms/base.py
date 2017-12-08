01c6e867709331227ad305f7b376dd4882810d69
Leslie_Yan <leslieyan0621@gmail.com>
2017-12-08 23:56:54 +0800

    [Fix] Rename inside functions

--- optimize_algorithms/base.py
+++ optimize_algorithms/base.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
@@ -8,12 +7,12 @@ from sklearn import metrics
 import concurrent.futures
 from functools import partial
 
-#TODO 写一个 recorder 类, 将每个 model里前20的param_name, param, perfrom_score都记录下来. 最终可以用来排序, 做ensemble
-class abstract_optimizer():
 
+# TODO 写一个 recorder 类, 将每个 model里前20的param_name, param, perfrom_score都记录下来. 最终可以用来排序, 做ensemble
+class abstract_optimizer():
     def __init__(self, model, X, y, param_names, param_bounds, param_types, metric, random_state):
         if not len(param_types) == len(param_bounds) == len(param_names):
-            raise("'param_types', 'param_bounds' and 'param_names' must have same length")
+            raise ("'param_types', 'param_bounds' and 'param_names' must have same length")
         else:
             self.param_types = param_types
             self.param_bounds = param_bounds
@@ -27,8 +26,7 @@ class abstract_optimizer():
         self.X, self.y = X, y
         self.metric = metric
 
-
-    def get_init_param(self, param_type, upper_bound, lower_bound, n):
+    def _get_init_param(self, param_type, upper_bound, lower_bound, n):
         if upper_bound > self.param_bound[1]:
             upper_bound = self.param_bound[1]
         if lower_bound < self.param_bound[0]:
@@ -58,7 +56,7 @@ class abstract_optimizer():
             raise ValueError("Value 'metric' is not specified")
         return score
 
-    def get_performance(self, param_name, param_list, fix_params=None):
+    def _get_performance(self, param_name, param_list, fix_params=None):
         if fix_params:
             pass
         else:
@@ -76,10 +74,10 @@ class abstract_optimizer():
         f[std == 0.0] = 0.0
         return f
 
-    def get_param(self, x, y):
+    def _get_param(self, x, y):
         g = GaussianProcessRegressor()
         g.set_params(**{'random_state': self.seed})
-        x = np.array(x).reshape(-1,1)
+        x = np.array(x).reshape(-1, 1)
         g.fit(x, y)
         mean, std = g.predict(x, return_std=True)
         ei = self.acquisition(mean=mean, std=std, best=0)
@@ -87,7 +85,7 @@ class abstract_optimizer():
         best = np.max(ei)
         return candidate, best
 
-    def get_str_param(self, param_name, param_list, fix_params=None):
+    def _get_str_param(self, param_name, param_list, fix_params=None):
         perform_scores = []
         if fix_params:
             pass
@@ -101,7 +99,7 @@ class abstract_optimizer():
             if self.metric == 'accuracy':
                 score = self.model.score(self.X_test, self.y_test)
             elif self.metric == 'auc':
-                pred_prob = self.model.predict_proba(self.X_test)[:,1]
+                pred_prob = self.model.predict_proba(self.X_test)[:, 1]
                 fpr, tpr, thresholds = metrics.roc_curve(self.y_test, pred_prob)
                 score = metrics.auc(fpr, tpr)
             else:
@@ -109,7 +107,6 @@ class abstract_optimizer():
             perform_scores.append(round(score, 4))
         opt_param = param_list[np.argmax(perform_scores)]
         return opt_param
-
 
     def opt_run(self, max_iter):
         params = {}
@@ -120,17 +117,17 @@ class abstract_optimizer():
                     param_name = self.param_names[i]
                     param_bound = self.param_bounds[i]
                     self.param_bound = self.param_bounds[i]
-                    best_param, best_ei = self.get_opt_param(param_type=param_type,
-                                                             param_name=param_name,
-                                                             param_bound=param_bound,
-                                                             max_iter=max_iter)
+                    best_param, best_ei = self._get_opt_param(param_type=param_type,
+                                                              param_name=param_name,
+                                                              param_bound=param_bound,
+                                                              max_iter=max_iter)
                     params[param_name] = best_param
                 elif param_type == 'str':
                     param_name = self.param_names[i]
                     param_list = self.param_bounds[i]
-                    opt_param = self.get_str_param(param_name=param_name,
-                                                   param_list=param_list,
-                                                   fix_params=params)
+                    opt_param = self._get_str_param(param_name=param_name,
+                                                    param_list=param_list,
+                                                    fix_params=params)
                     params[param_name] = opt_param
             return params
         elif len(self.param_types) == 0:
@@ -138,22 +135,20 @@ class abstract_optimizer():
         else:
             raise ValueError("'param_types', 'param_names' or 'param_bounds' is invalid")
 
-
-
-    def get_opt_param(self, param_type, param_name, param_bound, max_iter):
+    def _get_opt_param(self, param_type, param_name, param_bound, max_iter):
         if param_type == 'str':
             pass
         else:
             upper_bound = param_bound[1]
             lower_bound = param_bound[0]
             print('Initial parameters for {0}.'.format(param_name))
-            init_param = self.get_init_param(param_type=param_type,
-                                             upper_bound=upper_bound,
-                                             lower_bound=lower_bound,
-                                             n=100)
-            x, y = self.get_performance(param_name=param_name,
-                                        param_list=init_param)
-            candidate, best = self.get_param(x, y)
+            init_param = self._get_init_param(param_type=param_type,
+                                              upper_bound=upper_bound,
+                                              lower_bound=lower_bound,
+                                              n=100)
+            x, y = self._get_performance(param_name=param_name,
+                                         param_list=init_param)
+            candidate, best = self._get_param(x, y)
             if len(x) < 30:
                 best_param = candidate
                 best_ei = best
@@ -164,13 +159,13 @@ class abstract_optimizer():
                     new_upper = candidate * 1.2
                     new_lower = candidate * 0.8
                     try:
-                        new_param = self.get_init_param(param_type=param_type,
-                                                        lower_bound=new_lower,
-                                                        upper_bound=new_upper,
-                                                        n=30)
-                        x, y = self.get_performance(param_name=param_name,
-                                                    param_list=new_param)
-                        candidate, best = self.get_param(x, y)
+                        new_param = self._get_init_param(param_type=param_type,
+                                                         lower_bound=new_lower,
+                                                         upper_bound=new_upper,
+                                                         n=30)
+                        x, y = self._get_performance(param_name=param_name,
+                                                     param_list=new_param)
+                        candidate, best = self._get_param(x, y)
                         converge.add(param=candidate,
                                      ei=best)
                         if converge.check():
@@ -185,7 +180,6 @@ class abstract_optimizer():
 
 
 class Converge():
-
     def __init__(self):
         self.param = []
         self.ei = []
@@ -197,10 +191,10 @@ class Converge():
     def check(self):
         if len(self.ei) >= 5:
             times = 0
-            for i in [4,3,2,1]:
-                minuend = self.ei[-(i+1)]
+            for i in [4, 3, 2, 1]:
+                minuend = self.ei[-(i + 1)]
                 subtrahend = self.ei[-i]
-                rate = abs(subtrahend-minuend)/minuend
+                rate = abs(subtrahend - minuend) / minuend
                 if rate <= 0.02:
                     times += 1
             if times >= 3:
@@ -209,23 +203,3 @@ class Converge():
                 return False
         else:
             return False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
